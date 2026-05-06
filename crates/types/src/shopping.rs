1ac7f79d2ec3560cf8bf150e4434aecb0c00b773
snapiz <jonathan.lapiquonne@gmail.com>
2026-05-06 19:38:57 -0400

    feat(shopping-list): allow to generate from custom date (#549)

--- crates/types/src/shopping.rs
+++ crates/types/src/shopping.rs
@@ -2,7 +2,15 @@ use crate::recipe::Ingredient;
 
 #[evento::aggregator]
 pub enum Shopping {
-    Checked { ingredient: String },
-    Unchecked { ingredient: String },
-    Generated { ingredients: Vec<Ingredient> },
+    Checked {
+        ingredient: String,
+    },
+    Unchecked {
+        ingredient: String,
+    },
+    Generated {
+        ingredients: Vec<Ingredient>,
+        from_date: u64,
+        days: u8,
+    },
 }
