be61e8439470523439c3e4cbdae19430594e4846
jonathan <jonathan.lapiquonne@gmail.com>
2026-05-25 15:28:19 -0400

    fix(recipe): import not upsert

--- crates/core/src/recipe/root/import.rs
+++ crates/core/src/recipe/root/import.rs
@@ -4,6 +4,8 @@ use imkitchen_types::recipe::{
 };
 use validator::Validate;
 
+use super::UpdateInput;
+
 #[derive(Validate, Clone)]
 pub struct ImportInput {
     pub recipe_type: RecipeType,
@@ -35,6 +37,38 @@ impl<E: Executor + Clone> super::Module<E> {
     ) -> crate::Result<String> {
         input.validate()?;
         let request_by = request_by.into();
+
+        if let Some(existing_id) = self
+            .find_user_to_upsert(&request_by, input.origin.as_deref(), &input.name)
+            .await?
+        {
+            if let Some(existing) = self.load(&existing_id).await? {
+                if existing.owner_id == request_by {
+                    self.update(
+                        UpdateInput {
+                            id: existing_id.clone(),
+                            recipe_type: input.recipe_type,
+                            name: input.name,
+                            origin: input.origin,
+                            description: input.description,
+                            household_size: input.household_size,
+                            prep_time: input.prep_time,
+                            cook_time: input.cook_time,
+                            ingredients: input.ingredients,
+                            instructions: input.instructions,
+                            dietary_restrictions: input.dietary_restrictions,
+                            cuisine_type: input.cuisine_type,
+                            accepts_accompaniment: input.accepts_accompaniment,
+                            advance_prep: input.advance_prep,
+                        },
+                        &request_by,
+                    )
+                    .await?;
+
+                    return Ok(existing_id);
+                }
+            }
+        }
 
         Ok(evento::create()
             .event(&Imported {
